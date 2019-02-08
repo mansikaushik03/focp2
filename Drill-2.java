da8e1b5e8aff5e095fa1b77f072e54882a43fef4
mansikaushik03 <47098640+mansikaushik03@users.noreply.github.com>
2019-02-08 09:43:24 +0530

    added
    Drill-2

---
+++ Drill-2.java
@@ -0,0 +1,48 @@
+                 DRILL 2
+PROCEDURAL
+
+Int main(){
+Report report;
+AddName(card,1);
+addRollNo(card,1);
+addClass(card,1);
+addSection(card,1);
+addBatch(card,1);
+AddSubject(card,6);
+Grades(card,5);
+Total(card,1);
+Std::cout<<”result is ready”;
+Return 0;
+}
+
+
+
+
+
+
+
+
+OBJECT ORIENTED
+
+Int main(){
+Report report;
+Name name(1);
+RollNo rollno(1);
+Class class(1);
+Section section(1);
+Batch batch(1);
+Subject subject(5);
+Grades grades(5);
+Total total(1);
+Report add(name);
+Report.add(roll);
+Report.add(class);
+Report.add(section);
+Report.add(batch);
+Report.add(subjects);
+Report.add(grades);
+Report.add(total);
+Std::cout<<”result is ready”;
+return 0;
+}
+
